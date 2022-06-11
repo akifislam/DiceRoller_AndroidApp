1e87d088b40888ec806792c1888132ccf55b8a7d
Akif Islam <iamakifislam@gmail.com>
2022-06-11 16:43:14 +0600

    image added

--- app/src/main/java/com/akifislam/diceroller/MainActivity.kt
+++ app/src/main/java/com/akifislam/diceroller/MainActivity.kt
@@ -1,18 +1,28 @@
-// Dice Roller without Image
+// Dice Roller with Image
 
 package com.akifislam.diceroller
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+//    function to sum
+//    fun startSound(){
+//        val mp = MediaPlayer.create(this, R.raw.sound)
+//        mp.start()
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var rollButton: Button = findViewById(R.id.roll_button)
         var textView : TextView = findViewById(R.id.textView)
+        var imageViewdice : ImageView = findViewById(R.id.image_view_dice)
 
         var number = 0
 
@@ -20,7 +30,18 @@ class MainActivity : AppCompatActivity() {
 
         rollButton.setOnClickListener {
             number = (1..6).random()
+            imageViewdice.setImageResource(when(number){
+                1 -> R.drawable.dice1
+                2 -> R.drawable.dice2
+                3 -> R.drawable.dice3
+                4 -> R.drawable.dice4
+                5 -> R.drawable.dice5
+                else -> R.drawable.dice6
+            })
             textView.text = number.toString()
+
+
+
         }
 
 
